026bf91ba49209663a54fd9376e662e916117d2d
siyiwolf <39256744@qq.com>
2017-03-03 22:36:52 +0800

    【JDBC】数据库实现

--- usrRegLog/src/com/find/usr/pg/RegisterServlet.java
+++ usrRegLog/src/com/find/usr/pg/RegisterServlet.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 public class RegisterServlet extends HttpServlet{
 	private UsrManager usrManager = null;
+	private DaoManager daoManager = null;
 	
 	@Override
 	public void init() throws ServletException {
 		usrManager = UsrManager.getInstance();
+		daoManager = DaoManager.getInstance();
 		System.out.println("Register get data manage tool!!");
 	}
 
@@ -23,7 +25,7 @@ public class RegisterServlet extends HttpServlet{
 		String uName = req.getParameter("uname");
 		String uPassword = req.getParameter("password");
 		System.out.println(uName + "+" + uPassword);
-		if (usrManager == null)
+		/*if (usrManager == null)
 		{
 			System.out.println("Data initalization failed!");
 		}
@@ -37,12 +39,27 @@ public class RegisterServlet extends HttpServlet{
 		else
 		{
 			resp.sendRedirect("registerFail.html");
+		}*/
+		
+		if (daoManager == null)
+		{
+			System.out.println("Data initalization failed!");
+		}
+		
+		if (daoManager.insertAccount(new UserAccount(uName, uPassword)) == 0)
+		{
+			System.out.println("RegisterServlet add success!");
+			resp.sendRedirect("login.html");
+		}
+		else
+		{
+			resp.sendRedirect("registerFail.html");
 		}
 	}
 
 	@Override
 	public void destroy() {
-		System.out.println("Register Servlet Destory");
+		System.out.println("Register  Servlet Destory");
 		super.destroy();
 	}
 	
